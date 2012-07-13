d0f2738461876a3d0e4694a63f5cad93ed63e844
Michael Johnston <lastobelus@mac.com>
2012-07-12 18:01:10 -0700

    fixes to hellodb

--- hello_db.php
+++ hello_db.php
@@ -1,18 +1,24 @@
 <?php
   # touch Thu Jul 12 17:38
   $db=yaml_parse_file('db.yaml');
+        echo "<pre>";
   print_r($db);
   $connection = mysql_connect($db['host'], $db['username'], $db['password']);
-  mysql_select_db($db['name'], $connection);
+  mysql_select_db($db['database'], $connection);
 
+  print_r($connection);
 
-  $result = mysql_query('SELECT `current_timestamp` FROM `last_access` ORDER BY `current_timestamp` DESC LIMIT 1');
+  $result = mysql_query('SELECT `access_time` FROM `last_access` ORDER BY `access_time` DESC LIMIT 1');
+  print_r($result);
+
   $row = mysql_fetch_array($result);
   echo(print_r($row,true));
 
-  mysql_query('INSERT INTO `last_access` SET `current_timestamp` = \''.time().'\'');
+echo('INSERT INTO `last_access` SET `access_time` = \''.date('Y-m-d H:i:s').'\'');
+  mysql_query('INSERT INTO `last_access` SET `access_time` = \''.date('Y-m-d H:i:s').'\'');
 
+        echo "</pre>";
   echo 'Hello World MODIFIED. Today\'s date is ',date('d/m/Y h:i:s'),'.';
-  echo ' This script was last accessed at  ',date('d/m/Y h:i:s', $row['current_timestamp']),'';
+  echo ' This script was last accessed at  ', $row['access_time'],'';
 
 ?>
